c29c0d38531d186677004ec542c51a86ad8f4711
Stanislav Petrov <s.e.petrov@gmail.com>
2020-10-05 17:34:39 +0300

    Remove vendor

--- vendor/github.com/kubeless/kubeless/pkg/utils/configlocation.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-// ConfigLocation is a struct to store the location of kubeless configuration specific ConfigMap
-type ConfigLocation struct {
-	Name      string
-	Namespace string
-}
