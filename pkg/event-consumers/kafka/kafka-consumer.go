997d5f62dad8de67c257be8c2dbe0d2e758afcfb
Stanislav Petrov <s.e.petrov@gmail.com>
2020-10-13 11:50:47 +0300

    Add backoff (#33)
    * Add exponential backoff when consumer function fails
    
    * Tidy modulues
    
    * Add BACKOFF_INTERVAL

--- pkg/event-consumers/kafka/kafka-consumer.go
+++ pkg/event-consumers/kafka/kafka-consumer.go
@@ -22,8 +22,10 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/Shopify/sarama"
+	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 
@@ -31,11 +33,12 @@ import (
 )
 
 var (
-	stopM     map[string]chan struct{}
-	stoppedM  map[string]chan struct{}
-	consumerM map[string]bool
-	brokers   string
-	config    *sarama.Config
+	stopM      map[string]chan struct{}
+	stoppedM   map[string]chan struct{}
+	consumerM  map[string]bool
+	brokers    string
+	maxBackOff time.Duration
+	config     *sarama.Config
 )
 
 const clientID = "kubeless-kafka-trigger-controller"
@@ -56,6 +59,14 @@ func init() {
 	brokers = os.Getenv("KAFKA_BROKERS")
 	if brokers == "" {
 		brokers = defaultBrokers
+	}
+
+	if s := os.Getenv("BACKOFF_INTERVAL"); len(s) > 0 {
+		if d, err := time.ParseDuration(s); err == nil {
+			maxBackOff = d
+		} else {
+			logrus.Errorf("Failed to parse maximum back off interval BACKOFF_INTERVAL: %v", err)
+		}
 	}
 
 	config = sarama.NewConfig()
@@ -101,7 +112,7 @@ func createConsumerProcess(topic, funcName, ns, consumerGroupID string, clientse
 	}
 
 	ready := make(chan struct{})
-	consumer := NewConsumer(funcName, funcPort, ns, clientset, ready)
+	consumer := NewConsumer(funcName, funcPort, ns, clientset, ready, maxBackOff)
 	errchan := group.Errors()
 
 	go func() {
@@ -182,16 +193,18 @@ type Consumer struct {
 	ns        string
 	clientset kubernetes.Interface
 	ready     chan struct{}
+	backoff   time.Duration
 }
 
 // NewConsumer returns new consumer.
-func NewConsumer(funcName string, funcPort int, ns string, clientset kubernetes.Interface, ready chan struct{}) *Consumer {
+func NewConsumer(funcName string, funcPort int, ns string, clientset kubernetes.Interface, ready chan struct{}, backoff time.Duration) *Consumer {
 	return &Consumer{
 		clientset: clientset,
 		funcName:  funcName,
 		funcPort:  funcPort,
 		ns:        ns,
 		ready:     ready,
+		backoff:   backoff,
 	}
 }
 
@@ -214,6 +227,8 @@ func (c *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
 
 // ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
 func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	b := getBackOff(c.backoff)
+
 	for msg := range claim.Messages() {
 		req, err := utils.GetHTTPReq(c.funcName, c.funcPort, msg.Topic, c.ns, kafkatriggersNamespace, "POST", string(msg.Value))
 		if err != nil {
@@ -221,13 +236,39 @@ func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 			continue
 		}
 
-		if err = utils.SendMessage(req); err != nil {
-			logrus.Errorf("Failed to send message (namespace = %v function = %v topic = %v partition = %v offset = %v): %v", c.ns, c.funcName, msg.Topic, msg.Partition, msg.Offset, err)
-		} else {
-			logrus.Infof("Message sent successfully (namespace = %v function = %v topic = %v partition = %v offset = %v)", c.ns, c.funcName, msg.Topic, msg.Partition, msg.Offset)
+		err = utils.SendMessage(req)
+		session.MarkMessage(msg, "")
+
+		if err != nil {
+			d := b.NextBackOff()
+			logrus.Errorf("Failed to send message (namespace = %v function = %v topic = %v partition = %v offset = %v): %v: backing off for %v", c.ns, c.funcName, msg.Topic, msg.Partition, msg.Offset, err, d)
+			time.Sleep(d)
+			continue
 		}
 
-		session.MarkMessage(msg, "")
+		logrus.Infof("Message sent successfully (namespace = %v function = %v topic = %v partition = %v offset = %v)", c.ns, c.funcName, msg.Topic, msg.Partition, msg.Offset)
+		b.Reset()
 	}
 	return nil
+}
+
+type backOff interface {
+	NextBackOff() time.Duration
+	Reset()
+}
+
+type noopBackOff struct{}
+
+func (noopBackOff) NextBackOff() time.Duration { return 0 }
+func (noopBackOff) Reset()                     {}
+
+func getBackOff(maxBackOff time.Duration) backOff {
+	if maxBackOff < 1 {
+		return noopBackOff{}
+	}
+
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = 0 // ... so that b.NextBackOff() never returns backoff.Stop.
+	b.MaxInterval = maxBackOff
+	return b
 }
